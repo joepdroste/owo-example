0934a43ddf55a743b14c7e54e844b0b5fd87ff57
joepd <545067@student.saxion.nl>
2024-10-26 01:43:52 +0200

    yes

---
+++ src/main/java/com/github/joepdroste/owoTest/OwoTest.java
@@ -0,0 +1,10 @@
+package com.github.joepdroste.owoTest;
+
+import net.fabricmc.api.ModInitializer;
+
+public class OwoTest implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
