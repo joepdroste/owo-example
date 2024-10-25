90a710e14c339b2906f632584d4f253d01fade46
joepd <545067@student.saxion.nl>
2024-10-26 01:33:43 +0200

    yes

---
+++ src/client/java/com/github/joepdroste/owoTest/client/OwoTestClient.java
@@ -0,0 +1,10 @@
+package com.github.joepdroste.owoTest.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OwoTestClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
