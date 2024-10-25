726613558ff152399667d7070e8e07de45b49672
joepd <545067@student.saxion.nl>
2024-10-26 01:40:11 +0200

    yes

--- src/client/java/com/github/joepdroste/owoTest/client/OwoTestClient.java
+++ src/client/java/com/github/joepdroste/owoTest/client/OwoTestClient.java
@@ -1,10 +1,30 @@
 package com.github.joepdroste.owoTest.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
 public class OwoTestClient implements ClientModInitializer {
+    private static KeyBinding keyBinding;
 
     @Override
     public void onInitializeClient() {
+        keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.owo.menu",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_B,
+                "category.owo"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (keyBinding.wasPressed()) {
+                MinecraftClient.getInstance().setScreen(new MyScreen());
+            }
+        });
     }
 }
